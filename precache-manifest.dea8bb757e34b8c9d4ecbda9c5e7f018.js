34d1c7845f09a6ebce1dd350f0e01bf41120dd45
vikas Rathod <vikasrathod2014@gmail.com>
2020-08-24 15:00:41 +0530

    Updates

---
+++ precache-manifest.dea8bb757e34b8c9d4ecbda9c5e7f018.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c57265265a1fc6fb702d2b946a889b3",
+    "url": "/web_messenger/index.html"
+  },
+  {
+    "revision": "547fd0299b7273ae90bf",
+    "url": "/web_messenger/static/css/main.1c3df511.chunk.css"
+  },
+  {
+    "revision": "ff53547167d37289505f",
+    "url": "/web_messenger/static/js/2.7b03f92d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/web_messenger/static/js/2.7b03f92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547fd0299b7273ae90bf",
+    "url": "/web_messenger/static/js/main.4d635b5c.chunk.js"
+  },
+  {
+    "revision": "b342ea5c8a6f27a1adc0",
+    "url": "/web_messenger/static/js/runtime-main.99d6de32.js"
+  },
+  {
+    "revision": "97c00759d90d786d9b6096d274ad3e07",
+    "url": "/web_messenger/static/media/chat-background.97c00759.png"
+  },
+  {
+    "revision": "509191fad4a92ec6e3c5c3628e41ed36",
+    "url": "/web_messenger/static/media/nature.509191fa.jpg"
+  }
+]);
